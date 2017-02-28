9ee5329065cbef7f35de61d68458030ad1159811
Dataninja <info@dataninja.it>
2017-02-28 12:56:02 +0100

    First working version with ondata theme

---
+++ pivot-reverser.js
@@ -0,0 +1,57 @@
+$(function() {
+
+    var $input = $("#input"),
+        $output = $("#output");
+
+    function parse(text,field_separator,line_separator) {
+
+        var text = text || "",
+            field_separator = field_separator || "\t",
+            line_separator = line_separator || "\n";
+
+        return _.map(text.split(line_separator), function(line) {
+            return line.split(field_separator);
+        });
+
+    }
+
+    function reverse(input) {
+
+        var input = input || [],
+            output = [];
+
+        _.each(input, function(line,ln) {
+            if (ln) {
+                _.each(line, function(field,fn) {
+                    if (fn) {
+                        output.push([
+                            line[0],
+                            input[0][fn],
+                            field
+                        ]);
+                    }
+                });
+            }
+        });
+
+        return output;
+
+    }
+
+    function format(data,field_separator,line_separator) {
+        
+        var text = text || "",
+            field_separator = field_separator || "\t",
+            line_separator = line_separator || "\n";
+
+        return _.map(data, function(line) {
+            return line.join(field_separator);
+        }).join(line_separator);
+
+    }
+
+    $input.change(function() {
+        $output.val(format(reverse(parse($(this).val()))));
+    });
+
+});
